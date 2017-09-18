56443fe654c08b59a31e0ac19049ed888873c52c
Ahmad Ibrahim <ahmad.ib04@hotmail.com>
2017-09-18 21:21:47 +0200

    done

--- alarmclock/src/todo/SharedData.java
+++ alarmclock/src/todo/SharedData.java
@@ -10,64 +10,73 @@ public class SharedData {
 	private boolean alarmOn;
 	private int counter;
 	private int hhmmss;
-	
+
 	public SharedData(ClockOutput output) {
 		mutex = new MutexSem();
 		this.output = output;
 		hhmmss = 0;
 		alarmTime = Integer.MAX_VALUE;
 	}
-	private void alterClockTime(int time) {
-			hhmmss = time;
-			if(counter < 20 && counter > 0){
-				alarmTime = hhmmss;
-			}
-			if (hhmmss == alarmTime) {
-				counter--;
-				doAlarm20();
-			}	
-			String s = String.valueOf(hhmmss);
-			if(s.endsWith("60")){
-				hhmmss -= 60;
-				hhmmss += 100;
-			}
-			if(s.endsWith("6000")){
-				hhmmss -= 6000;
-				hhmmss += 10000;
-			}
-			if(hhmmss == 236000) hhmmss = 0;
-			output.showTime(hhmmss);
-		
-	}
-	
+
 	public void setAlarmTime(int time) {
 		mutex.take();
 		counter = 20;
 		alarmTime = time;
 		mutex.give();
 	}
-	
-	public void setAlarmFlag(boolean alarmOn){
+
+	public void setAlarmFlag(boolean alarmOn) {
 		mutex.take();
 		this.alarmOn = alarmOn;
 		mutex.give();
 	}
-	
-	private void doAlarm20() {
-		if (counter != 0 && alarmOn) {
-			output.doAlarm();
-		}
-	}
 
-	public void setTime(int time){
+	public void setTime(int time) {
 		mutex.take();
 		alterClockTime(time);
 		mutex.give();
 	}
-	
+
 	public void incrementClock() {
 		mutex.take();
 		alterClockTime(hhmmss + 1);
 		mutex.give();
+	}
+
+	private void alterClockTime(int time) {
+		hhmmss = time;
+		checkAlarm();
+		checkTime();
+	}
+
+	private void checkTime() {
+		String s = String.valueOf(hhmmss);
+		if (s.endsWith("60")) {
+			hhmmss -= 60;
+			hhmmss += 100;
+		}
+		if (s.endsWith("5960")) {
+			hhmmss -= 6000;
+			hhmmss += 10000;
+		}
+		if (hhmmss == 235959)
+			hhmmss = 0;
+		output.showTime(hhmmss);
+	}
+
+	private void checkAlarm() {
+		if (counter < 20 && counter > 0) {
+			alarmTime = hhmmss;
+		}
+		if (hhmmss == alarmTime) {
+			counter--;
+			doAlarm20();
+		}
+	}
+
+	private void doAlarm20() {
+		if (counter != 0 && alarmOn) {
+			output.doAlarm();
+		}
 	}
 }
